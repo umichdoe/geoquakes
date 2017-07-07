982d5d4508681b378c913da553c9fea023c4316a
ryan-barrett <ascending62@gmail.com>
2017-07-07 15:47:41 -0700

    started project

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,22 @@ var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/s
 
 $(document).ready(function() {
   console.log("Let's get coding!");
-  // CODE IN HERE!
+  $.ajax({
+      method:'GET',
+      url: weekly_quakes_endpoint,
+      dataType: 'json',
+      //data:responseData,
+      success: onSuccess,
+  }); //closes ajax
+function onSuccess(responseData){
+  console.log('is working');
+  console.log(responseData);
+  console.log(`${responseData.features.length} response data length`);
+  console.log("This includes responseData" + responseData.features[0].properties.title);
+  for (i=0; i< responseData.features.length; i++){
+    $('h1').append(`${responseData.features[i].properties.title}`);
+  }
 
-});
+}
+
+}); //closes documentReady
